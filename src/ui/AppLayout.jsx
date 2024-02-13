ed085c27575a2c3ebda5c024018219ca60c468b0
rohan-1112 <rohankalaskar1112@gmail.com>
2024-02-13 18:13:14 +0530

    Add AppLayout component and wrap all routes except Login and PageNotFound in it.

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,30 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import styled from "styled-components";
+
+function AppLayout() {
+  return (
+    <StyledAppLayout>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
+  );
+}
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100dvh;
+`;
+const Main = styled.main`
+  background-color: var(--color-grey-100);
+
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+export default AppLayout;
