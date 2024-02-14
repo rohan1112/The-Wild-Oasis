590b67fb56164b4e0f26042b52311a9ea45b521a
rohan-1112 <rohankalaskar1112@gmail.com>
2024-02-14 19:53:07 +0530

    Added React Query, React Query Devtools, and a QueryClientProvider to the App component. Added a CabinTable component to the Cabins page. Added a deleteCabin function to the apiCabins service.

---
+++ src/features/cabins/CabinRow.jsx
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
+import Button from "../../ui/Button";
+import { deleteCabin } from "../../services/apiCabins";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import Spinner from "../../ui/Spinner";
+
+const TableRow = styled.div`
+  display: grid;
+  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+  column-gap: 2.4rem;
+  align-items: center;
+  padding: 1.4rem 2.4rem;
+
+  &:not(:last-child) {
+    border-bottom: 1px solid var(--color-grey-100);
+  }
+`;
+
+const Img = styled.img`
+  display: block;
+  width: 6.4rem;
+  aspect-ratio: 3 / 2;
+  object-fit: cover;
+  object-position: center;
+  transform: scale(1.5) translateX(-7px);
+`;
+
+const Cabin = styled.div`
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: var(--color-grey-600);
+  font-family: "Sono";
+`;
+
+const Price = styled.div`
+  font-family: "Sono";
+  font-weight: 600;
+`;
+
+const Discount = styled.div`
+  font-family: "Sono";
+  font-weight: 500;
+  color: var(--color-green-700);
+`;
+
+function CabinRow({ cabin }) {
+  const { id, image, name, max_capacity, regular_price, discount } = cabin;
+
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: (id) => deleteCabin(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries("cabins");
+    },
+  });
+
+  return (
+    <TableRow role="row">
+      <Img src={image} alt="" />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {max_capacity} guests</div>
+      <Price>{formatCurrency(regular_price)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button onClick={() => mutate(id)} disabled={isDeleting}>
+        Delete
+      </button>
+    </TableRow>
+  );
+}
+
+export default CabinRow;
